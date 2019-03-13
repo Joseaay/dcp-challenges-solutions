e4e58ea1ece1a3045aae1dc3d2205f55524e9c53
Jose A. Ayllon <j.llamas@orwellg.com>
2019-03-13 11:48:28 +0000

    - Solve FOUC issue: https://github.com/facebook/create-react-app/issues/6399

--- src/index.js
+++ src/index.js
@@ -2,16 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
-function mainRender() {
-    ReactDOM.render(<App />, document.getElementById("root"));
-}
 
-if (process.env.NODE_ENV !== "production") {
-    // Workaround for https://github.com/facebook/create-react-app/issues/6399
-    // until it gets fixed upstream
-    setTimeout(() => {
-        mainRender();
-    }, 1000);
-} else {
-    mainRender();
-}
+ReactDOM.render(<App />, document.getElementById("root"));
