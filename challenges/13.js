a7c457686b5a1ba25639cbe893406e09c7c9b9cf
Jose A. Ayllon <j.llamas@orwellg.com>
2019-04-24 21:10:37 +0100

    Improves solution number 13 and add plain solution number 14

--- challenges/13.js
+++ challenges/13.js
@@ -4,37 +4,35 @@
 // This problem was asked by Amazon.
 //
 // Given an integer k and a string s, find the length of the
-// longest substring that contains at most k distinct characters.
+// longest substring that contains at most k distinct
+// characters.
 //
-// For example, given s = "abcba" and k = 2, the longest substring
-// with k distinct characters is "bcb".
+// For example, given s = "abcba" and k = 2, the longest
+// substring with k distinct characters is "bcb".
 // REMOVE::arguments:number,string
 
-export const dcpChallenge13 = (k, s) => {
-    let bestResult = "";
-    let bestResultAcc;
-    let splittedString = s.split("");
-    splittedString.forEach((firstLoopChar, index) => {
-        let usedChars = new Set();
-        usedChars.add(firstLoopChar);
-        bestResultAcc = firstLoopChar;
-        splittedString.slice(index + 1).some(secondLoopChar => {
-            if (!usedChars.has(secondLoopChar) && usedChars.size === k) {
-                if (bestResultAcc.length > bestResult.length) {
-                    bestResult = bestResultAcc;
-                    return true;
-                }
-            } else {
-                if (
-                    usedChars.size <= k &&
-                    bestResultAcc.length > bestResult.length
-                )
-                    bestResult = bestResultAcc;
-                bestResultAcc += secondLoopChar;
-                usedChars.add(secondLoopChar);
-            }
-            return false;
-        });
-    });
-    return bestResult;
-};
+export const dcpChallenge13 = (k, s) =>
+	s
+		.split("")
+		.map((loopElement1, i) => {
+			let distinctChars = new Set(loopElement1);
+			let stop = false;
+			return [loopElement1].concat(
+				s
+					.substr(i + 1)
+					.split("")
+					.filter(loopElement2 => {
+						if (distinctChars.size < k) {
+							distinctChars.add(loopElement2);
+							return loopElement2;
+						}
+						if (distinctChars.has(loopElement2) && !stop) {
+							return loopElement2;
+						} else {
+							stop = true;
+						}
+					})
+			);
+		})
+		.reduce((a, b) => (b.length > a.length ? b : a))
+		.join("");
--- src/components/common/Tabs/Tabs.jsx
+++ src/components/common/Tabs/Tabs.jsx
@@ -4,45 +4,45 @@ import { TabComponent } from "../Tab/Tab";
 import { contextConsumer } from "../../hoc/context-consumer.hoc";
 
 export const Tabs = ({ selectedTab }) => {
-    const [tabs] = useState(13);
-    const [tabPage, setTabPage] = useState(0);
+	const [tabs] = useState(14);
+	const [tabPage, setTabPage] = useState(0);
 
-    const nextPageButtonAvailable = () =>
-        Math.abs(tabPage) < Math.floor(tabs / 10) * 1000;
-    const prevPageButtonAvailable = () => tabPage < 0;
+	const nextPageButtonAvailable = () =>
+		Math.abs(tabPage) < Math.floor(tabs / 10) * 1000;
+	const prevPageButtonAvailable = () => tabPage < 0;
 
-    const prevPage = _ =>
-        prevPageButtonAvailable() ? setTabPage(tabPage + 1000) : null;
+	const prevPage = _ =>
+		prevPageButtonAvailable() ? setTabPage(tabPage + 1000) : null;
 
-    const nextPage = _ =>
-        nextPageButtonAvailable() ? setTabPage(tabPage - 1000) : null;
+	const nextPage = _ =>
+		nextPageButtonAvailable() ? setTabPage(tabPage - 1000) : null;
 
-    return (
-        <div className="tabs">
-            {prevPageButtonAvailable() && (
-                <div
-                    className="tabs__arrow tabs__arrow--left"
-                    onClick={prevPage}
-                />
-            )}
-            {[...Array(tabs).keys()].map((e, i) => (
-                <TabComponent
-                    key={`tab${i}`}
-                    i={i + 1}
-                    selected={selectedTab}
-                    style={{
-                        transform: `translateX(${tabPage}%)`
-                    }}
-                />
-            ))}
-            {nextPageButtonAvailable() && (
-                <div
-                    className="tabs__arrow tabs__arrow--right"
-                    onClick={nextPage}
-                />
-            )}
-        </div>
-    );
+	return (
+		<div className="tabs">
+			{prevPageButtonAvailable() && (
+				<div
+					className="tabs__arrow tabs__arrow--left"
+					onClick={prevPage}
+				/>
+			)}
+			{[...Array(tabs).keys()].map((e, i) => (
+				<TabComponent
+					key={`tab${i}`}
+					i={i + 1}
+					selected={selectedTab}
+					style={{
+						transform: `translateX(${tabPage}%)`
+					}}
+				/>
+			))}
+			{nextPageButtonAvailable() && (
+				<div
+					className="tabs__arrow tabs__arrow--right"
+					onClick={nextPage}
+				/>
+			)}
+		</div>
+	);
 };
 
 export const TabsComponent = contextConsumer(Tabs);
